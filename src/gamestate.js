73f237ef16f06d7f1f84316d00144727af56ead1
Florence Anipa <mawuaniipa@gmail.com>
2021-07-13 12:40:10 +0300

    Added background/clock functions

---
+++ src/gamestate.js
@@ -0,0 +1,14 @@
+const gameState = {
+
+  current: "INIT",
+  clock: 1,
+  tick(){
+    this.clock++;
+    console.log("clock", this.clock);
+    return this.clock;
+  }
+  
+}
+
+
+export default gameState;
